29ea4127947ff3a2f16b295051797cbe7074e0d2
Eric Wallace <ewallac2@umd.edu>
2018-09-16 17:06:58 -0400

    upd

--- validation_set_attack.py
+++ validation_set_attack.py
@@ -1,36 +1,17 @@
-import json
 import numpy as np
 import torch
-import itertools
-import pytablewriter
-
 import viz
 import utils
 from create_explainer import get_explainer
 from preprocess import get_preprocess
-from explainer.sparse import SparseExplainer
-
-import matplotlib
-matplotlib.use('Agg')
-from matplotlib import pyplot as plt
-from matplotlib import pylab as P
-
-
 import copy
 from collections import Iterable
-from scipy.stats import truncnorm
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
 import torchvision.transforms as transforms
-
-import torchvision.transforms.functional as F_trans
-
-from torchvision.models.inception import inception_v3
-
 import os
 import glob
-
 
 batch_size = 16
 
@@ -42,21 +23,14 @@ batch_size = 16
 #     ])
 
 def perturb(model, X, y=None, epsilon=2.0/255.0, protected=None):         
-    
-    #X_var = Variable(torch.from_numpy(X).cuda(), requires_grad=True, volatile=False)
-    #y_var = Variable(torch.LongTensor(y).cuda(), requires_grad=False, volatile=False)
-
-    #output = model(X_var)
-    ################################output = model(transf(X))
     output = model(X)
     if y is None:
         y = output.max(1)[1]
     loss = F.cross_entropy(output, y)        
-    loss.backward()
-    #grad_sign = X_var.grad.data.cpu().sign().numpy()
+    loss.backward()    
     grad_sign = X.grad.data.cpu().sign().numpy()
-    #################################################################### TODO, we use different maskes
-    protected = np.repeat(protected[np.newaxis, :, :], 3, axis=0)
+    #################################################################### TODO, use different masks?
+    protected = np.repeat(protected[:, np.newaxis, :, :], 3, axis=1)
     grad_sign = grad_sign * protected    
     perturbed_X = X.data.cpu().numpy() + epsilon * grad_sign                
     perturbed_X = np.clip(perturbed_X, 0, 1)    
@@ -76,6 +50,7 @@ def attackUnImportant(saliency, cutoff = 0.10):
         new_saliency.append(sal)
     new_saliency = np.stack(new_saliency)
     assert new_saliency.shape == (batch_size, height, width)
+    return new_saliency
 
 def run_protected(raw_images, cutoff):
     transf = get_preprocess('resnet50', 'sparse')
@@ -94,67 +69,66 @@ def run_protected(raw_images, cutoff):
     }
 
     configs = [
-        ['sparse', sparse_args],
+        #['sparse', sparse_args],
         ['vanilla_grad', None],
+        #['random', None], 
         ['grad_x_input', None],
         ['smooth_grad', None],
         ['integrate_grad', None],
+      #   ['gradcam', None],
+      #   ['guided_backprop', None],   # TODO, there is some bug with guided_backprop and deconv
+      #  ['real_time_saliency', None],
+      #   ['deconv', None],
+         # ['excitation_backprop', None],
+         # ['contrastive_excitation_backprop', None],
     ]
-    
-    
+        
     inputs = torch.stack([transf(x) for x in raw_images])
     inputs = Variable(inputs.cuda(), requires_grad=True)
+    scores = dict()
     for method_name, kwargs in configs:
         if method_name == "random":                    
-            saliency = torch.from_numpy(np.random.rand(batch_size,3,224,224)).cuda()                      
+            saliency = torch.from_numpy(np.random.rand(len(raw_images),3,224,224)).cuda()                      
             saliency = viz.VisualizeImageGrayscale(saliency)
-            protected_region = attackUnImportant(saliency.cpu().numpy(), cutoff=current_cutoff)
+            protected_region = attackUnImportant(saliency.cpu().numpy(), cutoff=cutoff)
         else:
             explainer = get_explainer(model, method_name, kwargs)
             saliency = explainer.explain(copy.deepcopy(inputs), None)
-            saliency = viz.VisualizeImageGrayscale(saliency)        
-            protected_region = attackUnImportant(saliency.cpu().numpy(), cutoff=current_cutoff)
-
-    exit()
-
-
-
-        # adversarial_image = perturb(model, copy.deepcopy(inp), protected = protected_region)                        
-        # original_prediction = model(inp).max(1)[1]        
-        # adversarial_prediction = model(adversarial_image).max(1)[1]           
-        # if (original_prediction.data.cpu().numpy()[0] == adversarial_prediction.data.cpu().numpy()[0]):
-        #     explainers_correct[idx] = explainers_correct[idx] + 1
-    	
-        # with open("protected_results.txt", "a") as text_file:
-        #     print("Adversary Can Modify: ", current_cutoff)
-        #     text_file.write('\n' + str(current_cutoff) + '\n' +'\n')
-        #     for idx, explainer_correct in enumerate(explainers_correct):
-        #         print(explainer_correct)
-        #         print("Method: ", explainers[idx], "Protected Accuracy: ", float(explainer_correct) / num_total)#float(num_total))           
-        #         text_file.write(str(explainers[idx]) + '\n')
-        #         text_file.write(str(float(explainer_correct) / num_total) + '\n')#float(num_total))   
+            saliency = viz.VisualizeImageGrayscale(saliency)       
+            protected_region = attackUnImportant(saliency.cpu().numpy(), cutoff=cutoff)
+    
+        adversarial_image = perturb(model, copy.deepcopy(inputs), protected = protected_region)                        
+        original_prediction = model(inputs).max(1, keepdim=True)[1]        
+        adversarial_prediction = model(adversarial_image).max(1, keepdim=True)[1]        
+        correct = original_prediction.eq(adversarial_prediction).sum().cpu().data.numpy()
+        scores[method_name] = float(correct / batch_size)
+    return scores
 
 if __name__ == '__main__':
-    cutoffs = [0, 10,20,30,40,50,60,70,80,90,100] # percentage adversary can see    
+    cutoffs = [10,20,30,40,50,60,70,80,90] # percentage adversary can see    
     for cutoff in cutoffs:
         image_path = '/fs/imageNet/imagenet/ILSVRC_val/**/*.JPEG'
-        image_files = list(glob.iglob(image_path, recursive=True))[:64]
+        image_files = list(glob.iglob(image_path, recursive=True))
+        np.random.seed(0)
+        np.random.shuffle(image_files)
+        image_files = image_files[:1000]
         indices = list(range(0, len(image_files), batch_size))
         all_scores = None
         for batch_idx, start in enumerate(indices):
-            if batch_idx > 1:
-                break
             batch = image_files[start: start + batch_size]
             raw_images = [viz.pil_loader(x) for x in batch]
             scores = run_protected(raw_images, cutoff)
             if all_scores is None:
                 all_scores = scores
                 continue
-            for method_name in scores:
-                for attack_name, values in scores[method_name].items():
-                    all_scores[method_name][attack_name] += values
+            for method_name in scores:                
+                all_scores[method_name] += scores[method_name]
 
-        for method_name in all_scores:
-            for attack_name, values in all_scores[method_name].items():
-                values = list(map(list, zip(*values)))
-                print(method_name, attack_name, [np.mean(x) for x in values])
+        with open("protected_results.txt", "a") as text_file:
+            print("Adversary Can Modify: ", cutoff)
+            text_file.write('\n' + str(cutoff) + '\n' +'\n')
+            for method_name in all_scores:
+                accuracy = all_scores[method_name] / float(len(image_files) / batch_size)
+                print(method_name, accuracy)
+                text_file.write(str(method_name) + '\n')
+                text_file.write(str(accuracy) + '\n')
