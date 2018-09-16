21809498507ef25c75fe21098943bebabd6e27df
Eric Wallace <eswallace@comcast.net>
2018-09-16 10:17:27 -0400

    blah

--- validation_set_attack.py
+++ validation_set_attack.py
@@ -31,6 +31,9 @@ from torchvision.models.inception import inception_v3
 import os
 import glob
 
+
+batch_size = 16
+
 # inv_normalize = transforms.Compose([         
 #     transforms.Normalize(mean=[-0.485/0.229, -0.456/0.224, -0.406/0.255],
 #         std=[1/0.229, 1/0.224, 1/0.255]),
@@ -61,79 +64,97 @@ def perturb(model, X, y=None, epsilon=2.0/255.0, protected=None):
     return X
 
 def attackUnImportant(saliency, cutoff = 0.10):               
+    # if cutoff == 0:
+    #     protected_percentile = -1  
+    batch_size, height, width = saliency.shape  
     saliency = np.abs(saliency)
-    protected_percentile = np.percentile(saliency, cutoff)
-    if cutoff == 0:
-        protected_percentile = -1
-    return saliency <= protected_percentile
+    new_saliency = []
+    for i in range(batch_size):
+        sal = saliency[i].copy()
+        protected_percentile = np.percentile(sal, cutoff)
+        sal = sal <= protected_percentile
+        new_saliency.append(sal)
+    new_saliency = np.stack(new_saliency)
+    assert new_saliency.shape == (batch_size, height, width)
 
-def attackImportant(saliency, cutoff = 0.10):               
-    saliency = np.abs(saliency)
-    protected_percentile = np.percentile(saliency, 100 - cutoff)
-    if cutoff == 0:
-        protected_percentile = -1
-    return saliency >= protected_percentile
-
-def VisualizeImageGrayscale(image_3d, percentile=99):
-    image_3d = np.abs(image_3d.squeeze())
-    image_2d = torch.sum(image_3d, dim=0)
-    image_2d = image_2d.numpy()    
-    vmax = np.percentile(image_2d, percentile)
-    vmin = np.min(image_2d)
-    return torch.from_numpy(np.clip((image_2d - vmin) / (vmax - vmin), 0, 1))
-
-def main():
+def run_protected(raw_images, cutoff):
+    transf = get_preprocess('resnet50', 'sparse')
     model = utils.load_model('resnet50')
+    model.eval()
     model.cuda()
-    vanilla_grad_explainer = get_explainer(model, 'vanilla_grad', None)
-    kwargs = {'hessian_coefficient': 1, 'lambda_l1': 1e4, 'lambda_l2': 0, 'n_iterations': 10} 
-    sparse_explainer = get_explainer(model, 'sparse', None)#kwargs)
-    smooth_grad_explainer = get_explainer(model, 'smooth_grad', None)
-    integrate_grad_explainer = get_explainer(model, 'integrate_grad', None)
-    sparse_integrate_grad_explainer = get_explainer(model, 'sparse_integrate_grad', None)
-    explainers = [smooth_grad_explainer, integrate_grad_explainer, vanilla_grad_explainer, 'random']#smooth_grad_explainer, integrate_grad_explainer, sparse_explainer]#, 'random']#, sparse_explainer]
-    transf = transforms.Compose([
-        transforms.Scale((224, 224)),
-        transforms.ToTensor(),        
-    ])
 
-    target = None          
-    image_path = '/fs/imageNet/imagenet/ILSVRC_val/'
-    cutoffs = [0, 10,20,30,40,50,60,70,80,90,100]
-    #cutoffs = [10, 20]
-    for current_cutoff in cutoffs:
-        num_total = 0
-        explainers_correct = [0] * len(explainers)
-        for filename in glob.iglob(image_path + '**/*.JPEG', recursive=True):
-            num_total = num_total + 1
-            raw_img = viz.pil_loader(filename)
-            inp = transf(raw_img)            ######################################### TODO ERIC KNOWS THIS        
-            inp = utils.cuda_var(inp.unsqueeze(0), requires_grad=True)
-            for idx, explainer in enumerate(explainers): 
-                if explainer == "random":                    
-                    saliency = torch.from_numpy(np.random.rand(3,224,224)).unsqueeze(0).cuda()                      
-                    saliency = VisualizeImageGrayscale(saliency)
-                    protected_region = attackUnImportant(saliency.cpu().numpy(), cutoff=current_cutoff)
-                else:
-                    saliency = explainer.explain(copy.deepcopy(inp), target)
-                    saliency = VisualizeImageGrayscale(saliency)        
-                    protected_region = attackUnImportant(saliency.cpu().numpy(), cutoff=current_cutoff)
+    sparse_args = {
+        'lambda_t1': 1,
+        'lambda_t2': 1,
+        'lambda_l1': 0,
+        'lambda_l2': 1e4,
+        'n_iterations': 10,
+        'optim': 'sgd',
+        'lr': 1e-1,
+    }
 
-                adversarial_image = perturb(model, copy.deepcopy(inp), protected = protected_region)                
-                
-                original_prediction = model(inp).max(1)[1]        
-                adversarial_prediction = model(adversarial_image).max(1)[1]           
-                if (original_prediction.data.cpu().numpy()[0] == adversarial_prediction.data.cpu().numpy()[0]):
-                    explainers_correct[idx] = explainers_correct[idx] + 1
+    configs = [
+        ['sparse', sparse_args],
+        ['vanilla_grad', None],
+        ['grad_x_input', None],
+        ['smooth_grad', None],
+        ['integrate_grad', None],
+    ]
+    
+    
+    inputs = torch.stack([transf(x) for x in raw_images])
+    inputs = Variable(inputs.cuda(), requires_grad=True)
+    for method_name, kwargs in configs:
+        if method_name == "random":                    
+            saliency = torch.from_numpy(np.random.rand(batch_size,3,224,224)).cuda()                      
+            saliency = viz.VisualizeImageGrayscale(saliency)
+            protected_region = attackUnImportant(saliency.cpu().numpy(), cutoff=current_cutoff)
+        else:
+            explainer = get_explainer(model, method_name, kwargs)
+            saliency = explainer.explain(copy.deepcopy(inputs), None)
+            saliency = viz.VisualizeImageGrayscale(saliency)        
+            protected_region = attackUnImportant(saliency.cpu().numpy(), cutoff=current_cutoff)
+
+    exit()
+
+
+
+        # adversarial_image = perturb(model, copy.deepcopy(inp), protected = protected_region)                        
+        # original_prediction = model(inp).max(1)[1]        
+        # adversarial_prediction = model(adversarial_image).max(1)[1]           
+        # if (original_prediction.data.cpu().numpy()[0] == adversarial_prediction.data.cpu().numpy()[0]):
+        #     explainers_correct[idx] = explainers_correct[idx] + 1
     	
-        with open("protected_results.txt", "a") as text_file:
-            print("Adversary Can Modify: ", current_cutoff)
-            text_file.write('\n' + str(current_cutoff) + '\n' +'\n')
-            for idx, explainer_correct in enumerate(explainers_correct):
-                print(explainer_correct)
-                print("Method: ", explainers[idx], "Protected Accuracy: ", float(explainer_correct) / num_total)#float(num_total))           
-                text_file.write(str(explainers[idx]) + '\n')
-                text_file.write(str(float(explainer_correct) / num_total) + '\n')#float(num_total))   
+        # with open("protected_results.txt", "a") as text_file:
+        #     print("Adversary Can Modify: ", current_cutoff)
+        #     text_file.write('\n' + str(current_cutoff) + '\n' +'\n')
+        #     for idx, explainer_correct in enumerate(explainers_correct):
+        #         print(explainer_correct)
+        #         print("Method: ", explainers[idx], "Protected Accuracy: ", float(explainer_correct) / num_total)#float(num_total))           
+        #         text_file.write(str(explainers[idx]) + '\n')
+        #         text_file.write(str(float(explainer_correct) / num_total) + '\n')#float(num_total))   
 
 if __name__ == '__main__':
-    main()    
+    cutoffs = [0, 10,20,30,40,50,60,70,80,90,100] # percentage adversary can see    
+    for cutoff in cutoffs:
+        image_path = '/fs/imageNet/imagenet/ILSVRC_val/**/*.JPEG'
+        image_files = list(glob.iglob(image_path, recursive=True))[:64]
+        indices = list(range(0, len(image_files), batch_size))
+        all_scores = None
+        for batch_idx, start in enumerate(indices):
+            if batch_idx > 1:
+                break
+            batch = image_files[start: start + batch_size]
+            raw_images = [viz.pil_loader(x) for x in batch]
+            scores = run_protected(raw_images, cutoff)
+            if all_scores is None:
+                all_scores = scores
+                continue
+            for method_name in scores:
+                for attack_name, values in scores[method_name].items():
+                    all_scores[method_name][attack_name] += values
+
+        for method_name in all_scores:
+            for attack_name, values in all_scores[method_name].items():
+                values = list(map(list, zip(*values)))
+                print(method_name, attack_name, [np.mean(x) for x in values])
