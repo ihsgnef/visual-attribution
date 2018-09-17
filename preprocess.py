0ba5c48ee08e91d8b7131c1693a3c2c9a4e06653
Eric Wallace <eswallace@comcast.net>
2018-09-17 09:11:55 -0400

    add cifar normalization

--- preprocess.py
+++ preprocess.py
@@ -26,39 +26,48 @@ class PatternPreprocess(object):
         return ret
 
 
-def get_preprocess(arch, method):
-    if arch == 'googlenet':
-        transf = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[123. / 255, 117. / 255, 104. / 255],
-                                 std=[1. / 255, 1. / 255, 1. / 255])
-        ])
-    elif arch == 'vgg16':
-        if method.find('pattern') != -1:  # pattern_net, pattern_lrp
-            transf = transforms.Compose([
-                PatternPreprocess((224, 224))
-            ])
-        else:
+def get_preprocess(arch, method, dataset='imagenet'):
+    if dataset == "imagenet":
+        if arch == 'googlenet':
             transf = transforms.Compose([
                 transforms.Resize((224, 224)),
                 transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+                transforms.Normalize(mean=[123. / 255, 117. / 255, 104. / 255],
+                                     std=[1. / 255, 1. / 255, 1. / 255])
             ])
-    elif arch == 'resnet50' or arch == 'resnet18' or 'softplus50':
-        if method == 'real_time_saliency':
-            normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5],
-                                             std=[0.5, 0.5, 0.5])
-        else:
-            normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                             std=[0.229, 0.224, 0.225])
+        elif arch == 'vgg16':
+            if method.find('pattern') != -1:  # pattern_net, pattern_lrp
+                transf = transforms.Compose([
+                    PatternPreprocess((224, 224))
+                ])
+            else:
+                transf = transforms.Compose([
+                    transforms.Resize((224, 224)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+                ])
+        elif arch == 'resnet50' or arch == 'resnet18' or 'softplus50':
+            if method == 'real_time_saliency':
+                normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                                                 std=[0.5, 0.5, 0.5])
+            else:
+                normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                 std=[0.229, 0.224, 0.225])
 
-        transf = transforms.Compose([
-            transforms.Resize((224, 224)),
+            transf = transforms.Compose([
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                #normalize #####################################################
+            ])
+    if dataset == "cifar10":
+        if arch == 'resnet50':    
+            normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),                
+            
+            transf = transforms.Compose([                
             transforms.ToTensor(),
             #normalize #####################################################
-        ])
+            ])
 
     return transf
 
