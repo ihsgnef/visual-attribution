b23b3e1dd26e9949b7e13eb8081b300721272d36
Eric Wallace <eswallace@comcast.net>
2018-09-24 12:31:53 -0400

    pictures

--- protected_attack_redo.py
+++ protected_attack_redo.py
@@ -14,6 +14,10 @@ import torchvision
 from explainers_redo import SparseExplainer, RobustSparseExplainer, \
     VanillaGradExplainer, IntegrateGradExplainer, SmoothGradExplainer, \
     LambdaTunerExplainer
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+from matplotlib import pylab as P
 
 def gray_out(model, X, protected=None):
         protected = np.repeat(protected[:, np.newaxis, :, :], 3, axis=1)
@@ -142,7 +146,7 @@ def attackUnImportant(saliency, cutoff = 10):
         k = int(cutoff / 100 * height * width) - 1
         sal = np.reshape(sal,(height*width))
         bottom_k = np.argpartition(sal, k)
-        sal = np.zeros_like(sal).astype(int)
+        sal = np.zeros_like(sal).astype(int)        
         for idx in bottom_k[:k]:
             sal[idx] = 1
         sal = np.reshape(sal, (height,width))
@@ -163,7 +167,7 @@ if __name__ == '__main__':
     explainers = [
         ('Vanilla', VanillaGradExplainer()),
         ('Random', None),
-        #('SmoothGrad', SmoothGradExplainer()),
+        ('SmoothGrad', SmoothGradExplainer()),
         #('Sparse', SparseExplainer()),
         #('Tuned_Sparse', LambdaTunerExplainer()),
         #('Random', None),
@@ -175,8 +179,8 @@ if __name__ == '__main__':
     for method_name, explainer in explainers:
         cutoff_scores[method_name] = [0] * 11
 
-    cutoffs = [0,1,2,3,4,5,6,7,8,9,10]#,20,30]#[0,10,20,30,40,50,60,70,80,90,100] # percentage adversary can see
-    num_images = 16#32
+    cutoffs = [10]#0,1,2,3,4,5,6,7,8,9,10]#,20,30]#[0,10,20,30,40,50,60,70,80,90,100] # percentage adversary can see
+    num_images = 1#16#32
 
     batch_size = 1#16
 
@@ -188,6 +192,9 @@ if __name__ == '__main__':
         original_confidence = F.softmax(forward_pass, dim=1)
         #confidence_for_prediction = original_confidence[original_prediction]#.max(1, keepdim=True)
         confidence_for_class = original_confidence.cpu().data.numpy()[0][original_prediction.cpu().data.numpy()][0][0]
+
+        raw_img = batch.cpu().numpy()[0]#viz.pil_loader(batch.cpu().numpy()[0])
+        all_saliency_maps = []                
 
         for method_name, explainer in explainers:
             if method_name == "Random":
@@ -202,27 +209,50 @@ if __name__ == '__main__':
                 #adversarial_image = single_step_perturb(model, copy.deepcopy(inputs), protected = protected_region)                                          
                 # adversarial_prediction = model(adversarial_image).max(1, keepdim=True)[1]
                 # correct = original_prediction.eq(adversarial_prediction).sum().cpu().data.numpy()
-                # gray_image = gray_out(model, copy.deepcopy(inputs), protected = protected_region)
-                # gray_confidence = F.softmax(model(gray_image), dim=1)
-                # gray_confidence_for_class = gray_confidence.cpu().data.numpy()[0][original_prediction.cpu().data.numpy()][0][0]
+                
+                gray_image = gray_out(model, copy.deepcopy(inputs), protected = protected_region)
+                gray_confidence = F.softmax(model(gray_image), dim=1)
+                gray_confidence_for_class = gray_confidence.cpu().data.numpy()[0][original_prediction.cpu().data.numpy()][0][0]
                 # print(confidence_for_class)
                 # print(gray_confidence_for_class)
                 # print(original_prediction[0])
                 # print(gray_confidence.max(1, keepdim=True))
                 # print()
-                # correct = confidence_for_class - gray_confidence_for_class
+                correct = confidence_for_class - gray_confidence_for_class
 
-                adversarial_image = iterative_perturb(model, copy.deepcopy(inputs), protected = protected_region)
-                adversarial_prediction = model(adversarial_image).max(1, keepdim=True)[1]
-                correct = original_prediction.eq(adversarial_prediction).sum().cpu().data.numpy()
+                # adversarial_image = iterative_perturb(model, copy.deepcopy(inputs), protected = protected_region)
+                # adversarial_prediction = model(adversarial_image).max(1, keepdim=True)[1]
+                # correct = original_prediction.eq(adversarial_prediction).sum().cpu().data.numpy()
 
-                cutoff_scores[method_name][int(cutoff)] += float(correct / batch_size)
+                cutoff_scores[method_name][int(cutoff/10)] += float(correct / batch_size)
+                            
+                protected_region = np.repeat(protected_region[:, np.newaxis, :, :], 3, axis=1)                
+                all_saliency_maps.append(batch.cpu().numpy()[0] * (1 - protected_region))# + (128 * protected_region))
+
+        plt.figure(figsize=(25, 15))
+        plt.subplot(3, 5, 1)
+        
+        raw_img = np.swapaxes(raw_img, 1,2)
+        plt.imshow(np.swapaxes(raw_img, 0,2))
+        plt.axis('off')
+        plt.title('Dog')
+        for i, saliency in enumerate(all_saliency_maps):                    
+            plt.subplot(3, 5, i + 2 + i // 4)                                
+            saliency = saliency[0]
+            saliency = np.swapaxes(saliency, 1,2)
+            plt.imshow(np.swapaxes(saliency, 0,2))#, cmap=P.cm.gray, vmin=0, vmax=1)
+
+            plt.axis('off')        
+            #plt.title("")
+        plt.tight_layout()
+        plt.savefig('output/protected_attack.png')
+
 
 
     for cutoff in cutoffs:
         print("Adversary Can Modify: ", cutoff)
         for method_name, explainer in explainers:
-            print(method_name, cutoff_scores[method_name][int(cutoff)] / (num_images / batch_size))
+            print(method_name, cutoff_scores[method_name][int(cutoff/10)] / (num_images / batch_size))
 
         # with open("protected_results.txt", "a") as text_file:
 
